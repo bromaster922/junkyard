66b00576100b7a0a935090da9b118bfd0678ede5
bromaster922 <118091335+bromaster922@users.noreply.github.com>
2026-07-01 12:51:00 -0600

    uff

--- songs/ui.hx
+++ songs/ui.hx
@@ -4,8 +4,8 @@ function postCreate() {
 	for (i in [accuracyTxt, scoreTxt, healthBar, healthBarBG])
 		i.visible = false;
 
-	missesTxt.y -= 150;
-	missesTxt.size = 48;
+	missesTxt.y -= 110;
+	missesTxt.size = 32;
 	missesTxt.color = FlxColor.BLACK;
 
 	hb = new FlxSprite(410,600).loadGraphic(Paths.image("game/bar"));
@@ -13,6 +13,10 @@ function postCreate() {
 	insert(0, hb);
 	hb.scale.set(1.5,1.5);
 	hb.antialiasing = false;
+}
+
+function postUpdate() {
+	missesTxt.text = "Misses: " + misses;
 }
 
 function onNoteHit(e) {
